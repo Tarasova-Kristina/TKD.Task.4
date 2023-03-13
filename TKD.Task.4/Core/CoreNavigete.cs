e876a7ce626461671643ad8f84d58e57d51fb4a4
VariosInstey <124655869+VariosInstey@users.noreply.github.com>
2023-03-13 21:47:10 +0300

    Добавьте файлы проекта.

---
+++ TKD.Task.4/Core/CoreNavigete.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace SAS.Task._4.Core
+{
+    public static class CoreNavigate
+    {
+        public static Frame? MyConnection { get; set; }
+    }
+}
---
+++ TKD.Task.4/View/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SAS.Task._4.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TKD.Task._4.View.Pages;
+
+namespace TKD.Task._4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
